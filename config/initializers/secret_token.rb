8e0a8f1b6d9583af4fb96b4829628914263e421a
Alex Bunardzic <alexbunardzic@gmail.com>
2014-02-19 12:26:10 -0800

    skeletal structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Softblue::Application.config.secret_key_base = 'd110a95a70a0c174217c0f572e01b78c28b94e6820f8bfb14fda0efdc0aa4aaa122738bfe98c5d635b59894654fffbd651fe76f02d14bd3212bc923105b1a986'
